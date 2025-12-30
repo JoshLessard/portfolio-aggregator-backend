7f31ed86831f45ed657c2b64d1619dafd8e694f7
Josh Lessard <jlessard@axonify.com>
2025-12-30 17:35:59 -0500

    Spring Boot Initializr initial commit.

---
+++ src/main/java/dev/joshlessard/portfolio/PortfolioAggregatorBackendApplication.java
@@ -0,0 +1,13 @@
+package dev.joshlessard.portfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfolioAggregatorBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortfolioAggregatorBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/joshlessard/portfolio/PortfolioAggregatorBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.joshlessard.portfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioAggregatorBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
