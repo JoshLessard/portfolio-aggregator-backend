ec4a70a03a7f27a6839eb1d1e9761ab315039df1
Josh Lessard <jlessard@axonify.com>
2026-01-02 15:34:36 -0500

    Add test data.

--- src/main/java/dev/joshlessard/portfolio/domain/InMemoryPortfolioService.java
+++ src/main/java/dev/joshlessard/portfolio/domain/InMemoryPortfolioService.java
@@ -1,14 +1,22 @@
 package dev.joshlessard.portfolio.domain;
 
+import static java.util.Arrays.asList;
 import static java.util.Collections.emptyList;
 
+import java.util.ArrayList;
 import java.util.List;
 
 // TODO Get rid of me
 public class InMemoryPortfolioService implements PortfolioService {
 
+    private final List<Portfolio> portfolios = new ArrayList<>();
+
+    public void addPortfolios( Portfolio... portfolios ) {
+        this.portfolios.addAll( asList( portfolios ) );
+    }
+
     @Override
     public List<Portfolio> getPortfolios() {
-        return emptyList();
+        return portfolios;
     }
 }
