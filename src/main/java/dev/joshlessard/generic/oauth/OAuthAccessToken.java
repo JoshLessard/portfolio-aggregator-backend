a2f7c75f024a03db70f7d57321c0e7294fa06e60
Josh Lessard <jlessard@axonify.com>
2026-01-04 22:04:56 -0500

    Remove TODO...we don't deserialize domain objects.

--- src/main/java/dev/joshlessard/generic/oauth/OAuthAccessToken.java
+++ src/main/java/dev/joshlessard/generic/oauth/OAuthAccessToken.java
@@ -2,7 +2,6 @@ package dev.joshlessard.generic.oauth;
 
 import java.util.Map;
 
-// TODO Watch primitives...will fail to deserialize if null
 public record OAuthAccessToken(
     String accessToken,
     String tokenType,
