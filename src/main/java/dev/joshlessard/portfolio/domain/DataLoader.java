8d765acfd3632efa0479b9a342ec7c113efaa7c3
Josh Lessard <jlessard@axonify.com>
2026-01-07 11:24:06 -0500

    Accidentally checked in a refresh token during testing.

--- src/main/java/dev/joshlessard/portfolio/domain/DataLoader.java
+++ src/main/java/dev/joshlessard/portfolio/domain/DataLoader.java
@@ -1,16 +1,11 @@
 package dev.joshlessard.portfolio.domain;
 
-import java.time.Instant;
-import java.time.temporal.ChronoUnit;
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.annotation.Profile;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
-import dev.joshlessard.generic.oauth.domain.OAuthAccessToken;
 import dev.joshlessard.generic.oauth.domain.OAuthAccessTokenRepository;
 
 // TODO Move me to oauth package
@@ -28,14 +23,6 @@ public class DataLoader {
 
     @EventListener( ApplicationReadyEvent.class )
     public void loadData() {
-        accessTokenRepository.setToken(
-            new OAuthAccessToken(
-                "iN70kIYYB1pDDFrRxAhdAgUf3I4mvWZm0",
-                "Bearer",
-                Instant.now().plus( 10, ChronoUnit.SECONDS ),
-                "_cy6kpsoiMGPAi-ZyG5pWjIvGAjIq4FE0",
-                Map.of( "api_server", "https://api01.iq.questrade.com/" )
-            )
-        );
+        // Inject real token here for testing
     }
 }
